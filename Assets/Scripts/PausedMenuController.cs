7a39a87ddb11c750a0bb60e5659b1ffcdd62b3da
orel-dayan <orel.dayan2@gmail.com>
2024-03-12 08:15:00 +0200

    add minimap and improve the menu popup

---
+++ Assets/Scripts/PausedMenuController.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class PauseMenuController : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu, pauseMenuMain, pauseMenuSettings;
+
+    [SerializeField] GameObject cam;
+    [SerializeField] GameObject weapon;
+    [SerializeField] bool isWeaponEnabledThisScene;
+
+
+    bool m_paused = false;
+
+    void Update()
+    {
+        var keyboard = Keyboard.current;
+        if (keyboard == null) return;
+        if (keyboard.tabKey.wasPressedThisFrame)
+        {
+            TogglePauseMenu();
+        }
+    }
+
+    void TogglePauseMenu()
+    {
+        if (m_paused)
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+            cam.GetComponent<CameraRotation>().enabled = true;
+            weapon.SetActive(isWeaponEnabledThisScene);
+            Time.timeScale = 1.0f;
+
+            pauseMenu.SetActive(false);
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.None;
+            cam.GetComponent<CameraRotation>().enabled = false;
+            Debug.Log("shutting off cam movement");
+            weapon.SetActive(false);
+            Cursor.visible = true;
+            Time.timeScale = 0.0f;
+
+            pauseMenu.SetActive(true);
+        }
+        m_paused = !m_paused;
+    }
+    public void SettingsBackOnClick()
+    {
+        pauseMenuSettings.SetActive(false);
+        pauseMenuMain.SetActive(true);
+    }
+
+
+    public void ResumeOnClick()
+    {
+        TogglePauseMenu();
+    }
+
+    public void SettingsOnClick()
+    {
+        pauseMenuMain.SetActive(false);
+        pauseMenuSettings.SetActive(true);
+    }
+
+    public void ExitToMainOnClick()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene("MainMenu");
+    }
+}
